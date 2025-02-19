a16db9fb06b5a235a3370a8f7e03786e461165d5
Thiago Benega <thiagobenega@gmail.com>
2025-02-19 14:54:18 -0300

    Interactivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { Separator } from "@/components/ui/separator";
+import Chat from "./components/Chat";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  console.log("session", session);
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+      {!session?.user && <div>You need to log in to use this chat.</div>}
+      {session?.user && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   );
 }
